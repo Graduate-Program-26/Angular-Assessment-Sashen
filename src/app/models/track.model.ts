b295f960d25505aca9f4b51eaf2cae7ff5d89408
Sashen <SGovindasamy@dvtsoftware.com>
2026-05-05 13:27:52 +0200

    fix: import zone.js for bootstrap and add cover_small to Track model

--- src/app/models/track.model.ts
+++ src/app/models/track.model.ts
@@ -4,5 +4,5 @@ export interface Track {
   readonly duration: number;
   readonly preview:  string;
   readonly artist:   { readonly id: number; readonly name: string; readonly picture_medium: string };
-  readonly album:    { readonly id: number; readonly title: string; readonly cover_medium: string };
+  readonly album:    { readonly id: number; readonly title: string; readonly cover_small: string; readonly cover_medium: string };
 }
