31a731d9f0e560dfdb3338226370962f29d0aba9
Sashen <SGovindasamy@dvtsoftware.com>
2026-05-05 13:27:51 +0200

    feat: add custom TitleStrategy for dynamic page titles

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,9 @@
-import { ApplicationConfig, provideZoneChangeDetection,} from '@angular/core';
-import { provideRouter, withComponentInputBinding, withViewTransitions } from '@angular/router';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter, withComponentInputBinding, withViewTransitions, TitleStrategy } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { appRoutes } from './routes/app.routes';
 import { errorInterceptor } from '@core/interceptors/error.interceptor';
+import { MoonBeatsTitleStrategy } from '@core/strategies/page-title.strategy';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -16,5 +17,6 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(
       withInterceptors([errorInterceptor]),
     ),
+    { provide: TitleStrategy, useClass: MoonBeatsTitleStrategy },
   ],
 };
