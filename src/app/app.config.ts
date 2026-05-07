4cad906ba56d5c38a9f861caed5b48cfdab29394
Sashen <SGovindasamy@dvtsoftware.com>
2026-05-07 23:57:04 +0200

    feat: register provideAuth0 in app.config and update auth model

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,8 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withComponentInputBinding, withViewTransitions, TitleStrategy } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideAuth0 } from '@auth0/auth0-angular';
+import { environment } from '../environments/environment';
 import { appRoutes } from './routes/app.routes';
 import { errorInterceptor } from '@core/interceptors/error.interceptor';
 import { MoonBeatsTitleStrategy } from '@core/strategies/page-title.strategy';
@@ -14,9 +16,19 @@ export const appConfig: ApplicationConfig = {
       withComponentInputBinding(),
       withViewTransitions(),
     ),
+
     provideHttpClient(
       withInterceptors([errorInterceptor]),
     ),
+
+    provideAuth0({
+      domain: environment.auth0Domain,
+      clientId: environment.auth0ClientId,
+      authorizationParams: {
+        redirect_uri: window.location.origin,
+      },
+    }),
+
     { provide: TitleStrategy, useClass: MoonBeatsTitleStrategy },
   ],
 };
