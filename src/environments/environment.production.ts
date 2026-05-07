6037d64558076e1755eaeac841336f11b66946b5
Sashen <SGovindasamy@dvtsoftware.com>
2026-05-07 23:57:04 +0200

    chore: add auth0Domain and auth0ClientId to environment files

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,6 +1,8 @@
 export const environment = {
   production: true,
   deezerApiBaseUrl: '/api/deezer',
+  auth0Domain:   'dev-0oipuo6x1d6r2q1m.us.auth0.com',
+  auth0ClientId: 'oHwosjm9hi4RDkG5j8qaz8fvljXqOA1q',
 } as const;
 
 Object.freeze(environment);
