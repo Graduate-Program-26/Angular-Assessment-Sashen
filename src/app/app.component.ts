4bbb2f071777f72f2294e5c9c60f7cbbe2319449
Sashen <SGovindasamy@dvtsoftware.com>
2026-05-07 20:12:56 +0200

    feat: add bottom nav and sidebar, fix seek bar full-width with gold progress fill

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PlayerBarComponent } from '@shared/components/player-bar/player-bar.component';
 import { NavbarComponent } from '@shared/components/navbar/navbar.component';
-
+import { BottomNavComponent } from '@shared/components/bottom-nav/bottom-nav.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, PlayerBarComponent, NavbarComponent],
+  imports: [RouterOutlet, PlayerBarComponent, NavbarComponent, BottomNavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
